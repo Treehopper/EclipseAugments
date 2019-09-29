5ebe351a1b41b8b7f000e6013d3c3034ca73f3c2
Max Hohenegger <github.com@hohenegger.eu>
2019-09-29 12:11:27 +0200

    Filter redundant mixed XML nodes, getting rid of text nodes.

--- xsdemftools/eu.hohenegger.xsdemftools.ui/src/eu/hohenegger/xsdemftools/ui/handler/XmlDiffEngine.java
+++ xsdemftools/eu.hohenegger.xsdemftools.ui/src/eu/hohenegger/xsdemftools/ui/handler/XmlDiffEngine.java
@@ -2,7 +2,9 @@ package eu.hohenegger.xsdemftools.ui.handler;
 
 import org.eclipse.emf.compare.diff.DefaultDiffEngine;
 import org.eclipse.emf.compare.diff.FeatureFilter;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.xml.type.XMLTypePackage;
 
 public class XmlDiffEngine extends DefaultDiffEngine {
 
@@ -13,6 +15,14 @@ public class XmlDiffEngine extends DefaultDiffEngine {
 			@Override
 			public boolean checkForOrderingChanges(EStructuralFeature feature) {
 				return false;
+			}
+
+			@Override
+			protected boolean isIgnoredAttribute(EAttribute attribute) {
+				return attribute == XMLTypePackage.eINSTANCE.getAnyType_Mixed() ||
+						attribute == XMLTypePackage.eINSTANCE.getXMLTypeDocumentRoot_Mixed() ||
+						attribute == XMLTypePackage.eINSTANCE.getXMLTypeDocumentRoot_Text() ||
+						super.isIgnoredAttribute(attribute);
 			}
 		};
 	}
